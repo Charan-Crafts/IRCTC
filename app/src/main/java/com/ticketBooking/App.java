65e776640526171fcfe9f2eab70b4c121a157384
charan-crafts <mangalacharancharan@gmail.com>
2025-09-29 13:32:45 +0530

    first commit

--- app/src/main/java/com/ticketBooking/App.java
+++ app/src/main/java/com/ticketBooking/App.java
@@ -1,28 +1,8 @@
 package com.ticketBooking;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ticketBooking.Entites.Train;
-import com.ticketBooking.Entites.Users;
-import com.ticketBooking.services.TrainServices;
-import com.ticketBooking.utils.UserUtils;
+public  class App{
 
-import java.io.File;
-import java.sql.Time;
-import java.util.*;
-
-public class App {
-
-
-    public static void main(String[] args) throws Exception {
-
-        System.out.println("Welcome Book my Train App");
-
-        TrainServices trainServices = new TrainServices();
-
-        trainServices.findTrains("Delhi","Kanpur");
-
-        trainServices.bookTicket(86769,"Delhi","Kanpur","charan@gmail.com");
+    public static void main(String[] args) {
+        System.out.println("Hello");
     }
 }
